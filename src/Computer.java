243c07ef9730f71131bc51cdaa8009e12be33fd6
sengama <146013628+sengama@users.noreply.github.com>
2023-10-04 17:01:07 +0300

    Computer

---
+++ src/Computer.java
@@ -0,0 +1,25 @@
+import edu.tekwill.academy.computer.components.HardDrive;
+import edu.tekwill.academy.computer.components.Motherboard;
+import edu.tekwill.academy.computer.components.Processor;
+import edu.tekwill.academy.computer.peripherals.Keyboard;
+import edu.tekwill.academy.computer.peripherals.Monitor;
+import edu.tekwill.academy.computer.peripherals.Mouse;
+
+
+public class Computer {
+    public class components {
+        Processor processor;
+
+        Motherboard motherboard;
+
+        HardDrive hardDrive;
+
+        Monitor monitor;
+
+        Mouse mouse;
+
+        Keyboard keyboard;
+
+    }
+
+}
